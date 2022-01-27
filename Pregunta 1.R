73b531d2500db1204ba422681191e893ee6f24d9
Ivan Zuñiga Quiroz <ivan.zuniga@usach.cl>
2022-01-27 20:39:34 -0300

    Pregunta N°1 casi lista

--- Pregunta 1.R
+++ Pregunta 1.R
@@ -1,10 +1,10 @@
 library(tidyr)
 library(dplyr)
 library(leaps)
-library (boot)
-library (bootES)
-library (ggpubr)
-library (simpleboot)
+library(boot)
+library(bootES)
+library(ggpubr)
+library(simpleboot)
 
 
 # PREGUNTA N°1
@@ -18,12 +18,11 @@ library (simpleboot)
 set.seed(531)
 
 # Se lee el archivo de entrada.
-datos <- read.csv(file.choose(), encoding = "UTF-8", sep = ";")
+datos <- read.csv2(file.choose(),head=TRUE ,sep=";", stringsAsFactors = TRUE)
+
 
 # Se selecciona la casa Hufflepuff
 muestra <- datos %>% filter(casa == "Hufflepuff")
-# SE extrae el tamaño de la muestra
-n <- length(muestra)
 
 # Hipotesis:
 # Hipotesis nula    (H0): Los estudaintes de Hufflepuff tuvieron 12 puntos menos durante
@@ -32,13 +31,14 @@ n <- length(muestra)
 #                         durante el primer trimestre que en el segundo trimestre
 
 # Se selecciona el primer trimestre
-trim1 <- muestra %>% select(trim1)
+trimestre1 <- muestra$trim1
 # Se selecciona el segundo trimestre  
-trim2 <- muestra %>% select(trim2)
+trimestre2 <- muestra$trim2
 
 # Se calcula la media de ambas muestras
-media_trim1 <- mean(trim1)
-media_trim2 <- mean(trim2)
+
+media_trim1 <- mean(trimestre1)
+media_trim2 <- mean(trimestre2)
 
 # Diferencia entre medias muestrales
 diferencia <- media_trim1 - media_trim2
@@ -46,6 +46,17 @@ diferencia <- media_trim1 - media_trim2
 # Se utiliza el alfa a = 0,05
 alfa <- 0.05
 
+# Se crea la distribucion bootstrap
+bootstrap <- 5000
+distribucion_boot<-two.boot(trimestre1,trimestre2, FUN = mean, R = bootstrap)
 
+# Se establece un valor nulo igual a 12.
+valor_nulo <- 12
+desplazamiento <- mean(distribucion_boot[["t"]]-valor_nulo)
+distribucion_nula <-(distribucion_boot[["t"]]-desplazamiento)
 
+# Determina el valor P.
+p <- (sum(abs(distribucion_nula)>abs(diferencia))+1)/(bootstrap +1)
+cat("El valor p es:",p)
 
+# Conclusion
