83be0a9ca53e3ed63e01e0084a9dbf23e5c6dc10
Ivan Zuñiga Quiroz <ivan.zuniga@usach.cl>
2022-01-27 20:47:50 -0300

    Pregunta 3 terminada, pregunta 2 recien creada

---
+++ Pregunta 3.R
@@ -0,0 +1,26 @@
+# Pregunta N°3
+# Proponga un ejemplo interesante y novedoso (no mencionado en clase ni que 
+# aparezca en las lecturas dadas),relacionado con el sentir de los santiaguinos 
+# ante el aumento de la violencia de la delincuencia, donde se pueda usar un 
+# modelo de regresión lineal múltiple para predecir una variable de respuesta. 
+# Indiqué cuáles serían las variables involucradas en su ejemplo 
+# (con sus respectivos tipos y posibles valores).
+
+#ejemplo: 
+# Debido al miedo que se empieza a sentir en la region metropolitana debido al
+# fuerte aumento de la violencia utilizada por los delincuentes para cometer sus
+# fechorías, se a estado intentando crear un modelo predictivo el cual ayude 
+# a encontrar donde son los lugares mas probables para que estos delincuentes 
+# aparezcan. por lo que para poder resolver esto, se decidieron 2 variables 
+# muy importantes las cuales no tienen una gran  relacion una con la otra, 
+
+# las variables son: 
+# existencia de camaras en una calle(si o no,variable 
+# dicotomica) y la distancia a un reten de carabineros(metros)
+# lo que se busca es encontrar la relacion que tienen ambas variables con la 
+# decisión y posibilidad de que realicen fechorias en ese vecindario
+
+# h0: No existe diferencia en la delincuencia con la cantidad de 
+#     camaras o distancia a comisarias.
+# h1: si existe una diferencia de la delincuencia dependiendo de 
+#     la cantidad de camaras y o distancia a comisarias.
---
+++ Prengunta 2.R
@@ -0,0 +1,1 @@
+# Pregunta N°2
